d4fea452c502673c84b2ef55ed60551d6d1338d0
kayamen123 <fenri.mintardja@student.umn.ac.id>
2020-11-15 21:26:56 +0700

    Detail and Styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },  {
+    path: 'detail',
+    loadChildren: () => import('./pages/detail/detail.module').then( m => m.DetailPageModule)
   },
+
 ];
 
 @NgModule({
